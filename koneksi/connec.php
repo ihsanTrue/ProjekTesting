c2089f6ca9c918c2c43097b110b659eeb349dc4b
SutrisDevPemula <sutris.devpemula@gmail.com>
2019-11-03 23:15:40 +0800

    menambah file koneksi

---
+++ koneksi/connec.php
@@ -0,0 +1,14 @@
+<?php
+$url = "localhost";
+$user = "root";
+$password = "";
+$dataBase = "sirent";
+
+$con = mysqli_connect($url, $user, $password, $dataBase);
+
+if ($con->connect_error){
+
+    echo "gagal Koneksi ke databases : (" . $con-> connect_error. ")";
+}else{
+    echo "Koneksi Berhasil";
+}
